f90d0475aa5b26649aeb8c1f87f7bcc0018d3f8d
Pinapplekat <elijah.ryerson@gmail.com>
2024-06-02 18:16:05 -0700

    fix

--- main.lua
+++ main.lua
@@ -3,10 +3,10 @@ local p = get("p")
 local test = "Rizzler"
 
 local res = fetch({
-    url = "http://127.0.0.1:7999/all",
-    method = "GET",
+    url = "http://127.0.0.1:7999/new",
+    method = "POST",
     headers = { ["Content-Type"] = "application/json" }
-    -- body = '{ "name": "' .. test .. '"}'
+    body = '{ "name": "' .. test .. '"}'
 })
 
 p.set_content(res)
